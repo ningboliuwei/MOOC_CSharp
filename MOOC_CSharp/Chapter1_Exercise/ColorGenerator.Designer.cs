7a159ab5a1b3cfd195235925a41a7f34304e95e5
ningboliuwei <liuwei@nbu.edu.cn>
2014-12-04 15:04:11 +0800

    第一次提交

---
+++ MOOC_CSharp/Chapter1_Exercise/ColorGenerator.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace Chapter1_Exercise
+{
+	partial class ColorGenerator
+	{
+		/// <summary>
+		/// 必需的设计器变量。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清理所有正在使用的资源。
+		/// </summary>
+		/// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows 窗体设计器生成的代码
+
+		/// <summary>
+		/// 设计器支持所需的方法 - 不要
+		/// 使用代码编辑器修改此方法的内容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.btnGenerateColor = new System.Windows.Forms.Button();
+			this.btnRandomColor = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("微软雅黑", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.label1.Location = new System.Drawing.Point(12, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(135, 27);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "红（0~255）";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("微软雅黑", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.label2.Location = new System.Drawing.Point(12, 55);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(135, 27);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "绿（0~255）";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("微软雅黑", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.label3.Location = new System.Drawing.Point(12, 101);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(135, 27);
+			this.label3.TabIndex = 2;
+			this.label3.Text = "蓝（0~255）";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(152, 14);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(100, 23);
+			this.textBox1.TabIndex = 3;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(152, 60);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(100, 23);
+			this.textBox2.TabIndex = 4;
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(152, 105);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(100, 23);
+			this.textBox3.TabIndex = 5;
+			// 
+			// label4
+			// 
+			this.label4.BackColor = System.Drawing.Color.White;
+			this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.label4.Location = new System.Drawing.Point(258, 15);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(115, 113);
+			this.label4.TabIndex = 6;
+			// 
+			// btnGenerateColor
+			// 
+			this.btnGenerateColor.Location = new System.Drawing.Point(12, 142);
+			this.btnGenerateColor.Name = "btnGenerateColor";
+			this.btnGenerateColor.Size = new System.Drawing.Size(240, 33);
+			this.btnGenerateColor.TabIndex = 7;
+			this.btnGenerateColor.Text = "生成颜色";
+			this.btnGenerateColor.UseVisualStyleBackColor = true;
+			// 
+			// btnRandomColor
+			// 
+			this.btnRandomColor.Location = new System.Drawing.Point(258, 142);
+			this.btnRandomColor.Name = "btnRandomColor";
+			this.btnRandomColor.Size = new System.Drawing.Size(115, 33);
+			this.btnRandomColor.TabIndex = 8;
+			this.btnRandomColor.Text = "随机颜色";
+			this.btnRandomColor.UseVisualStyleBackColor = true;
+			// 
+			// ColorGenerator
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(385, 188);
+			this.Controls.Add(this.btnRandomColor);
+			this.Controls.Add(this.btnGenerateColor);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Name = "ColorGenerator";
+			this.Text = "颜色生成器";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Button btnGenerateColor;
+		private System.Windows.Forms.Button btnRandomColor;
+	}
+}
+
