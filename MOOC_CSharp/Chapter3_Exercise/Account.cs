fb8edeb32e783908613ce019d670d464be65a698
ningboliuwei <liuwei@nbu.edu.cn>
2014-12-27 15:42:46 +0800

    第三章作业

---
+++ MOOC_CSharp/Chapter3_Exercise/Account.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter3_Exercise
+{
+	public class Account
+	{
+		private double money; //decimal money;
+		private string id;
+		private string pwd;
+		//string name;
+
+		public Account() 
+		{
+			
+		}
+
+		public Account(string id, string pwd, double money)
+		{
+			this.id = id;
+			this.pwd = pwd;
+			this.money = money;
+		}
+
+		public double getMoney()
+		{
+			return money;
+		}
+
+		public void setMoney(double val)
+		{
+			this.money = val;
+		}
+
+		public string getId()
+		{
+			return id;
+		}
+
+		public void setId(string id)
+		{
+			this.id = id;
+		}
+
+		public string getPwd()
+		{
+			return pwd;
+		}
+
+		public void setPwd(string pwd)
+		{
+			this.pwd = pwd;
+		}
+
+		public bool SaveMoney(double money)
+		{
+			if (money < 0) return false; //卫语句
+
+			this.money += money;
+			return true;
+		}
+
+		public bool WithdrawMoney(double money)
+		{
+			if (this.money >= money)
+			{
+				this.money -= money;
+				return true;
+			}
+
+			return false;
+		}
+
+		public bool IsMatch(string id, string pwd)
+		{
+			return id == this.id && pwd == this.pwd;
+		}
+	}
+}
---
+++ MOOC_CSharp/Chapter3_Exercise/CreditCardAccount.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter3_Exercise
+{
+	class CreditCardAccount:Account
+	{
+		//额度
+		public double limit;
+		//积分
+		public long score;
+
+		public CreditCardAccount(string id, string pwd, double money, double limit) : base()
+		{
+			this.limit = limit;
+		}
+
+		/// <summary>
+		/// 改变信用卡额度
+		/// </summary>
+		/// <param name="limit">新的额度</param>
+		public void SetCreditLimit(double limit)
+		{
+			this.limit = limit;
+		}
+	}
+}
+
