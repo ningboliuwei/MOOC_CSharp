ad19c002d2821abc21a8b8cb177dad0ab33cd423
LIU Wei <liuwei@nbu.edu.cn>
2015-11-24 10:12:58 +0800

    add WeatherForecast,
    搞定有所有城市相关代码
    还差显示部分

---
+++ MOOC_CSharp/Chapter10_Exercise/WeatherForecast/frmMain.Designer.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WeatherForecast
+{
+	partial class frmMain
+	{
+		/// <summary>
+		/// 必需的设计器变量。
+		/// </summary>
+		private IContainer components = null;
+
+		/// <summary>
+		/// 清理所有正在使用的资源。
+		/// </summary>
+		/// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows 窗体设计器生成的代码
+
+		/// <summary>
+		/// 设计器支持所需的方法 - 不要修改
+		/// 使用代码编辑器修改此方法的内容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.cmbProvince = new ComboBox();
+			this.label1 = new Label();
+			this.label2 = new Label();
+			this.cmbCity = new ComboBox();
+			this.btnQuery = new Button();
+			this.gridView = new DataGridView();
+			((ISupportInitialize)(this.gridView)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// cmbProvince
+			// 
+			this.cmbProvince.DropDownStyle = ComboBoxStyle.DropDownList;
+			this.cmbProvince.FormattingEnabled = true;
+			this.cmbProvince.Location = new Point(76, 21);
+			this.cmbProvince.Name = "cmbProvince";
+			this.cmbProvince.Size = new Size(121, 26);
+			this.cmbProvince.TabIndex = 0;
+			this.cmbProvince.SelectedIndexChanged += new EventHandler(this.cmbProvince_SelectedIndexChanged);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new Font("宋体", 12F, FontStyle.Regular, GraphicsUnit.Point, ((byte)(134)));
+			this.label1.Location = new Point(12, 21);
+			this.label1.Name = "label1";
+			this.label1.Size = new Size(58, 24);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "省份";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new Font("宋体", 12F, FontStyle.Regular, GraphicsUnit.Point, ((byte)(134)));
+			this.label2.Location = new Point(203, 23);
+			this.label2.Name = "label2";
+			this.label2.Size = new Size(58, 24);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "城市";
+			// 
+			// cmbCity
+			// 
+			this.cmbCity.DropDownStyle = ComboBoxStyle.DropDownList;
+			this.cmbCity.FormattingEnabled = true;
+			this.cmbCity.Location = new Point(267, 24);
+			this.cmbCity.Name = "cmbCity";
+			this.cmbCity.Size = new Size(121, 26);
+			this.cmbCity.TabIndex = 3;
+			// 
+			// btnQuery
+			// 
+			this.btnQuery.Font = new Font("宋体", 12F, FontStyle.Regular, GraphicsUnit.Point, ((byte)(134)));
+			this.btnQuery.Location = new Point(394, 12);
+			this.btnQuery.Name = "btnQuery";
+			this.btnQuery.Size = new Size(122, 43);
+			this.btnQuery.TabIndex = 4;
+			this.btnQuery.Text = "查询天气";
+			this.btnQuery.UseVisualStyleBackColor = true;
+			this.btnQuery.Click += new EventHandler(this.btnQuery_Click);
+			// 
+			// gridView
+			// 
+			this.gridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.gridView.Location = new Point(13, 78);
+			this.gridView.Name = "gridView";
+			this.gridView.RowTemplate.Height = 30;
+			this.gridView.Size = new Size(997, 459);
+			this.gridView.TabIndex = 5;
+			// 
+			// frmMain
+			// 
+			this.AutoScaleDimensions = new SizeF(9F, 18F);
+			this.AutoScaleMode = AutoScaleMode.Font;
+			this.ClientSize = new Size(1022, 549);
+			this.Controls.Add(this.gridView);
+			this.Controls.Add(this.btnQuery);
+			this.Controls.Add(this.cmbCity);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.cmbProvince);
+			this.Name = "frmMain";
+			this.Text = "天气查询器";
+			this.Load += new EventHandler(this.frmMain_Load);
+			((ISupportInitialize)(this.gridView)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private ComboBox cmbProvince;
+		private Label label1;
+		private Label label2;
+		private ComboBox cmbCity;
+		private Button btnQuery;
+		private DataGridView gridView;
+	}
+}
+
---
+++ MOOC_CSharp/Chapter10_Exercise/WeatherForecast/frmMain.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace WeatherForecast
+{
+	public partial class frmMain : Form
+	{
+		List<LocationRecord> locationList = new List<LocationRecord>();
+		List<YahooCityCodeRecord> yahooCityCodeList = new List<YahooCityCodeRecord>();
+
+		public frmMain()
+		{
+			InitializeComponent();
+		}
+
+		//城市与省份对应记录类（第三方）
+		class LocationRecord
+		{
+			public string LocationSn;
+			public string LocationName;
+			public string LocationCode;
+			public string Province;
+		}
+
+		//城市名与 Yahoo Weather 代码对应记录类
+		class YahooCityCodeRecord
+		{
+			public string CityName;
+			public string YahooCityCode;
+		}
+
+		//从 mobile.weather.com.cn 获取所有城市与省份的对应数据
+		public void LoadLocationList()
+		{
+			string url = "http://mobile.weather.com.cn/js/citylist.xml";
+			XDocument doc = XDocument.Load(url);
+
+			//通过 LINQ 将数据添加到 locationList 这个泛型中
+			locationList = (from r in doc.Descendants("d")
+							select new LocationRecord
+							{
+								LocationSn = r.Attribute("d1").Value,
+								LocationName = r.Attribute("d2").Value,
+								LocationCode = r.Attribute("d3").Value,
+								Province = r.Attribute("d4").Value
+							}).ToList();
+		}
+
+		//将 Yahoo Weather 中不存在的城市剔除
+		public void FilterLocations()
+		{
+			locationList = (from l in locationList
+							where (from c in yahooCityCodeList
+								   select c.CityName).ToArray().Contains(l.LocationName)
+							select l).ToList();
+		}
+
+		private void btnQuery_Click(object sender, EventArgs e)
+		{
+			//获取数据后显示在 GridView 中
+			gridView.DataSource = GetYahooWeather(GetYahooCityCode(cmbCity.Text)).Tables[0];
+		}
+
+		//将省份显示到下拉列表中
+		private void BindProvinceComboBox()
+		{
+			//从（过滤后的）城市列表中选取所有的省份名称
+			var provinces = from l in locationList
+							select new
+							{
+								l.Province
+							};
+
+			cmbProvince.DisplayMember = "Province";
+			cmbProvince.DataSource = provinces.Distinct().ToList();
+		}
+
+		//根据省份名，在城市下拉列表中显示该省份对应的城市
+		private void BindCityComboBox(string province)
+		{
+			//在城市列表中找到所有对应省份为指定省份的记录
+			var cities = from c in locationList
+						 where c.Province == province
+						 select new
+						 {
+							 City = c.LocationName,
+							 Code = c.LocationCode
+						 };
+
+			cmbCity.DisplayMember = "City";
+			cmbCity.ValueMember = "Code";
+			cmbCity.DataSource = cities.Distinct().ToList();
+		}
+
+		//从网上找到的数据文件中根据城市名获取相应的 Yahoo Weather 城市代码数据
+		private void LoadCityCodeList()
+		{
+			string path = Application.StartupPath + "\\CityCodes.txt";
+
+			using (StreamReader sr = new StreamReader(path, Encoding.Default))
+			{
+				while (sr.Peek() >= 0)
+				{
+					string line = sr.ReadLine();
+					if (line != null)
+					{
+						yahooCityCodeList.Add(new YahooCityCodeRecord { CityName = line.Split(' ')[0], YahooCityCode = line.Split(' ')[1] });
+					}
+				}
+			}
+		}
+
+		//根据城市名获取 Yahoo Weather 城市代码
+		private string GetYahooCityCode(string cityName)
+		{
+			string s = (from c in yahooCityCodeList
+						where c.CityName == cityName
+						select c.YahooCityCode).First();
+			return s;
+		}
+
+		private void frmMain_Load(object sender, EventArgs e)
+		{
+			LoadLocationList();
+			LoadCityCodeList();
+			FilterLocations();
+			BindProvinceComboBox();
+			BindCityComboBox(locationList[0].Province);
+		}
+
+		private void cmbProvince_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			BindCityComboBox(cmbProvince.Text);
+		}
+
+		//以下为唐大仕老师例子中的代码
+		public static DataSet GetYahooWeather(string cityCode)
+		{
+			string strXml = "http://xml.weather.yahoo.com/forecastrss?p=" + cityCode;
+
+			XmlDocument Weather = new XmlDocument();
+			Weather.Load(strXml);
+
+			XmlNamespaceManager namespacemanager = new
+				XmlNamespaceManager(Weather.NameTable);
+			namespacemanager.AddNamespace("yweather",
+				"http://xml.weather.yahoo.com/ns/rss/1.0");
+			XmlNodeList nodes = Weather.SelectNodes("/rss/channel/item/yweather:forecast", namespacemanager);
+
+			//XmlNodeList nodes = Weather.GetElementsByTagName("forecast", "http://xml.weather.yahoo.com/ns/rss/1.0");
+
+			DataSet dstWeather = new DataSet();
+			DataTable dtblWeather = new DataTable("Weather");
+			dstWeather.Tables.Add(dtblWeather);
+
+			dstWeather.Tables["Weather"].Columns.Add("Date", typeof(string));
+			dstWeather.Tables["Weather"].Columns.Add("Week", typeof(string));
+			dstWeather.Tables["Weather"].Columns.Add("Weather", typeof(string));
+			dstWeather.Tables["Weather"].Columns.Add("Tlow", typeof(string));
+			dstWeather.Tables["Weather"].Columns.Add("Thigh", typeof(string));
+
+			if (nodes.Count > 0)
+			{
+				foreach (XmlNode node in nodes)
+				{
+					DataRow drowWeather = dstWeather.Tables["Weather"].NewRow();
+
+					drowWeather["Date"] = EmonthToCmonth(node.SelectSingleNode
+						("@date").Value);
+					drowWeather["Week"] = EweekToCweek(node.SelectSingleNode
+						("@day").Value) + "(" + node.SelectSingleNode("@day").Value
+										  + ")";
+					drowWeather["Weather"] = node.SelectSingleNode("@text").Value;
+					drowWeather["Tlow"] = FToC(int.Parse(node.SelectSingleNode
+						("@low").Value)) + "℃";
+					drowWeather["Thigh"] = FToC(int.Parse(node.SelectSingleNode
+						("@high").Value)) + "℃";
+
+					dstWeather.Tables["Weather"].Rows.Add(drowWeather);
+				}
+
+				return dstWeather;
+			}
+			DataRow drowNone = dstWeather.Tables["Weather"].NewRow();
+			drowNone["Week"] = "None";
+			drowNone["Weather"] = "None";
+			drowNone["Tlow"] = "None";
+			drowNone["Thigh"] = "None";
+
+			dstWeather.Tables["Weather"].Rows.Add(drowNone);
+			return dstWeather;
+
+			return dstWeather;
+		}
+
+		/**/
+
+		/// <summary>
+		/// 从华氏转换成摄氏
+		/// </summary>
+		/// <param name="f">华氏度</param>
+		/// <returns></returns>
+		private static string FToC(int f)
+		{
+			return Math.Round((f - 32) / 1.8, 1).ToString();
+		}
+
+		/**/
+
+		/// <summary>
+		/// 从星期英文缩写转汉字
+		/// </summary>
+		/// <param name="strEweek">星期的英文缩写</param>
+		/// <returns></returns>
+		private static string EweekToCweek(string strEweek)
+		{
+			switch (strEweek)
+			{
+				case "Mon":
+					return "星期一";
+					break;
+				case "Tue":
+					return "星期二";
+					break;
+				case "Wed":
+					return "星期三";
+					break;
+				case "Thu":
+					return "星期四";
+					break;
+				case "Fri":
+					return "星期五";
+					break;
+				case "Sat":
+					return "星期六";
+					break;
+				case "Sun":
+					return "星期日";
+					break;
+				default:
+					return "传参错误";
+					break;
+			}
+		}
+
+		/**/
+
+		/// <summary>
+		/// 从月英文缩写转汉字
+		/// </summary>
+		/// <param name="strReplace">需要替换的年月日</param>
+		/// <returns></returns>
+		private static string EmonthToCmonth(string strReplace)
+		{
+			return Convert.ToDateTime(strReplace).ToString("yyyy年MM月dd日");
+		}
+	}
+}
